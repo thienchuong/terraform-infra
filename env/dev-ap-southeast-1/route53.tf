14fe21434416783094ebe20dd9bc1720dd1d6346
chuong.nguyen <thienchuong004@gmail.com>
2024-12-14 19:47:29 +0700

    update

--- env/dev-ap-southeast-1/route53.tf
+++ env/dev-ap-southeast-1/route53.tf
@@ -21,7 +21,7 @@ module "zones" {
 # verify owner of domain chuong.click
 resource "aws_route53_record" "chuong-click" {
   name    = "${module.chuong-click.distinct_domain_names}"
-  records = ["${module.chuong-click.validation_route53_record_fqdns.[0]}"]
+  records = ["${module.chuong-click.validation_route53_record_fqdns}"]
   ttl     = "300"
   type    = "CNAME"
   zone_id = module.zones.route53_zone_zone_id["chuong.click"]
