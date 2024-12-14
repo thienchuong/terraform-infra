2e13faff29544009b9a271ff060c79afa4c0ffa7
chuong.nguyen <thienchuong004@gmail.com>
2024-12-14 15:01:11 +0700

    add oidc provider

---
+++ env/common-ap-southeast-1/oidc.tf
@@ -0,0 +1,10 @@
+module "github-oidc" {
+  source  = "terraform-module/github-oidc-provider/aws"
+  version = "~> 2.2.1"
+
+  create_oidc_provider = true
+  create_oidc_role     = true
+
+  repositories              = ["thienchuong/backend", "thienchuong/frontend"]
+  oidc_role_attach_policies = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"]
+}
