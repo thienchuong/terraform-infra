6838d24a07f5f803b1f964118a686a0c4094f46b
chuong.nguyen <thienchuong004@gmail.com>
2024-12-15 18:58:12 +0700

    request cert ssl and validation domain

--- env/dev-ap-southeast-1/acm.tf
+++ env/dev-ap-southeast-1/acm.tf
@@ -1,43 +1,43 @@
-# provider "aws" {
-#   alias = "route53"
-# }
+provider "aws" {
+  alias = "route53"
+}
 
-# provider "aws" {
-#   alias = "acm"
-# }
-# module "chuong-click" {
-#   source  = "terraform-aws-modules/acm/aws"
-#   version = "~> 4.0"
+provider "aws" {
+  alias = "acm"
+}
+module "chuong-click" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
 
-#   domain_name            = "chuong.click"
-#   subject_alternative_names = [
-#     "chuong.click",
-#     "*.chuong.click"
-#   ]
+  domain_name            = "chuong.click"
+  subject_alternative_names = [
+    "chuong.click",
+    "*.chuong.click"
+  ]
 
-#   create_route53_records = false
-#   validation_method      = "DNS"
+  create_route53_records = false
+  validation_method      = "DNS"
 
-#   validation_record_fqdns = module.chuong-click-record.validation_route53_record_fqdns
+  validation_record_fqdns = module.chuong-click-record.validation_route53_record_fqdns
 
-#   tags = {
-#     Env       = local.env
-#     Terraform = "true"
-#   }
-# }
+  tags = {
+    Env       = local.env
+    Terraform = "true"
+  }
+}
 
-# module "chuong-click-record" {
-#   source  = "terraform-aws-modules/acm/aws"
-#   version = "~> 4.0"
+module "chuong-click-record" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "~> 4.0"
 
-#   providers = {
-#     aws = aws.route53
-#   }
+  providers = {
+    aws = aws.route53
+  }
 
-#   create_certificate          = false
-#   create_route53_records_only = true
-#   validation_method = "DNS"
-#   distinct_domain_names = module.chuong-click.distinct_domain_names
-#   zone_id = module.zones.route53_zone_zone_id["chuong.click"]
-#   acm_certificate_domain_validation_options = module.chuong-click.acm_certificate_domain_validation_options
-# }
+  create_certificate          = false
+  create_route53_records_only = true
+  validation_method = "DNS"
+  distinct_domain_names = module.chuong-click.distinct_domain_names
+  zone_id = module.zones.route53_zone_zone_id["chuong.click"]
+  acm_certificate_domain_validation_options = module.chuong-click.acm_certificate_domain_validation_options
+}
