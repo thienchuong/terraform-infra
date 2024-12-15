d95469bf67bc89da62d48d5632fd05e9aa100e53
chuong.nguyen <thienchuong004@gmail.com>
2024-12-15 19:08:08 +0700

    only 1 natgw

--- env/dev-ap-southeast-1/network.tf
+++ env/dev-ap-southeast-1/network.tf
@@ -12,13 +12,13 @@ module "eks-vpc" {
       public_subnets         = ["10.12.80.0/20", "10.12.96.0/20", "10.12.112.0/20"]
       database_subnets       = ["10.12.48.0/20", "10.12.64.0/20"]
       private_subnets        = ["10.12.0.0/20", "10.12.16.0/20", "10.12.32.0/20"]
-      enable_nat_gateway     = true
-      single_nat_gateway     = true
-      one_nat_gateway_per_az = false
-      enable_dns_hostnames   = true
-      enable_dns_support     = true
     }
   ]
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
   cluster_name = local.cluster_name
   env          = local.env
 }
